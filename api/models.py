1aaf25086390e96c2127e768d34966d386e43c94
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 14:28:24 +0100

    feat: create Tag and Job models

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -14,9 +16,40 @@ class Category(models.Model):
 
 class Location(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = "locations"
+
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False, blank=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "tags"
+
+
+class Job(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="jobs")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="jobs")
+    locations = models.ManyToManyField(Location, related_name="jobs")
+    title = models.CharField(max_length=100, null=False, blank=False)
+    slug = models.SlugField(max_length=100, null=False, blank=False)
+    url = models.URLField(null=True, blank=True)
+    description = models.TextField(null=False, blank=False)
+    image = models.ImageField(upload_to='uploads/jobs', null=False, blank=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
