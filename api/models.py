c5e9b8d27121043b01e3af64cb0937216bafea5e
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 12:43:13 +0100

    feat: create migration and category model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    icon = models.ImageField(upload_to='uploads/categories', null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
