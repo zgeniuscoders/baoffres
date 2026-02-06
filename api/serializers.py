3ddd2dded1c48e9a15c6bcc51f395be8e7e26d9b
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 14:28:53 +0100

    feat: create TagSerializer and Jobs serialiezers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from api.models import Category, Location
+from api.models import Category, Location, Tag, Job
 
 
 class AddCategorySerializer(serializers.ModelSerializer):
@@ -22,3 +23,78 @@ class LocationSerializer(serializers.ModelSerializer):
         model = Location
         fields = ('id', 'name', 'created_at', 'updated_at')
         read_only_fields = ('created_at', 'updated_at')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'created_at', 'updated_at')
+        read_only_fields = ('created_at', 'updated_at')
+
+
+class AddJobSerializer(serializers.ModelSerializer):
+    category_id = (
+        serializers.PrimaryKeyRelatedField(
+            write_only=True,
+            source="category",
+            queryset=Category.objects.all())
+    )
+
+    locations = serializers.ListField(
+        write_only=True,
+        child=serializers.PrimaryKeyRelatedField(
+            queryset=Location.objects.all(),
+        )
+    )
+
+    tags = serializers.ListField(
+        write_only=True,
+        child=serializers.CharField()
+    )
+
+    class Meta:
+        model = Job
+        fields = ('locations', 'tags', 'title', 'url', 'image', 'category_id')
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags')
+        locations = validated_data.pop('locations')
+
+        job = Job.objects.create(**validated_data)
+
+        job.locations.set(locations)
+
+        tag_objs = []
+        for tag in tags:
+            find_tag, new_tag = Tag.objects.get_or_create(name=tag.strip().lower())
+            tag_objs.append(find_tag)
+
+        job.tags.set(tag_objs)
+        return job
+
+
+class JobListSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    owner = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Job
+        fields = ('id', 'title', 'slug', 'description', 'image', 'category', 'owner', 'created_at', 'updated_at')
+
+
+class JobSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    locations = LocationSerializer(read_only=True, many=True)
+    tags = TagSerializer(read_only=True, many=True)
+    owner = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Job
+        fields = ('id', 'title', 'slug', 'description', 'image', 'locations', 'category', 'owner', 'created_at',
+                  'updated_at', 'tags')
