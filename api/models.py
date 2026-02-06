d8390be3119d699bfafafb128df56c224c13f613
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 12:57:24 +0100

    feat: add Location model

--- api/models.py
+++ api/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=False, blank=False)
     icon = models.ImageField(upload_to='uploads/categories', null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
