c477e84c32cccdd3cbbc464c5c1b4ced5cb60409
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 12:58:39 +0100

    feat: add cache strategy to invalidate location data cache

--- api/signals.py
+++ api/signals.py
@@ -4,7 +4,14 @@ from django.dispatch import receiver
 
 from api.models import Category
 
+
 @receiver([post_save, post_delete], sender=Category)
 def invalidate_categories_cache(sender, instance, **kwargs):
     cache.delete_many("*categories*")
     cache.delete_many("*category*")
+
+
+@receiver([post_save, post_delete], sender=Category)
+def invalidate_location_cache(sender, instance, **kwargs):
+    cache.delete_many("*location_list*")
+    cache.delete_many("*location_detail*")
