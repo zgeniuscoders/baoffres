0ae60d31341bcce95f9be70b9cde89beb3832a2f
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 12:57:59 +0100

    feat: add LocationSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from api.models import Category
+from api.models import Category, Location
 
 
 class AddCategorySerializer(serializers.ModelSerializer):
@@ -15,3 +15,10 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'icon', 'created_at', 'updated_at')
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ('id', 'name', 'created_at', 'updated_at')
+        read_only_fields = ('created_at', 'updated_at')
