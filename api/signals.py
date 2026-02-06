66bda41f36c505fb2869984cdeabecb7a864610a
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 12:45:12 +0100

    feat: add cache strategy invalidation

---
+++ api/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save, post_delete
+from django.core.cache import cache
+from django.dispatch import receiver
+
+from api.models import Category
+
+@receiver([post_save, post_delete], sender=Category)
+def invalidate_categories_cache(sender, instance, **kwargs):
+    cache.delete_many("*categories*")
+    cache.delete_many("*category*")
