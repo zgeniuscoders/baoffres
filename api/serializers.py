375db4edf3cadd301f72798d8531b8bcdadda39d
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-06 19:41:49 +0100

    feat: override the create method and hash password in  UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -28,7 +28,15 @@ class LocationSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'password')
+        write_only_fields = ('password',)
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class TagSerializer(serializers.ModelSerializer):
