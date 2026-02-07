c1e690471a37d345a2e25651dd22eb1772873f42
zgeniusecoders <zgeniusecoders@gmail.com>
2026-02-07 12:23:49 +0100

    chore: add static url

--- core/settings.py
+++ core/settings.py
@@ -175,6 +175,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # <-- obligatoire pour collectstatic
+STATIC_URL = "/static/"
+
 MEDIA_URL = "/uploads/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
